2b5e7150d70b5dea29814f91b4a1adead54c793f
Ray Victor <ray7551@gmail.com>
2023-02-17 22:48:06 +0800

    add note data function

---
+++ src/crossbell.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+// get all post from https://indexer.crossbell.io/v1/characters/38333/feed?cursor=0&limit=20&includeCharacterMetadata=true
+//  https://indexer.crossbell.io/v1/characters/38333/feed/follow?limit=20&type=POST_NOTE&includeCharacterMetadata=true
+export async function getAllNotes(cursor=null, limit=20, includeCharacterMetadata=false) {
+  const response = await axios.get(`https://indexer.crossbell.io/v1/characters/38333/feed/follow?limit=${limit}${cursor ? '&cursor=' + cursor : ''}&includeCharacterMetadata=${includeCharacterMetadata}`);
+
+  return response.data.list.map(item => {
+    return {
+      title: item.note.metadata.title,
+      author: item.note.character.metadata.name,
+      content: item.note.metadata.content.content,
+      time: item.note.createdAt,
+      tags: item.note.metadata.content.tags,
+      cursor: item.cursor
+    };
+  });
+}
+
+// get notes of character 
+export function getNotesByCharacter(id, cursor=0, limit=20, includeCharacterMetadata=false) {
+  return axios.get(`https://indexer.crossbell.io/v1/characters/${id}/feed?cursor=${cursor}&limit=${limit}&includeCharacterMetadata=${includeCharacterMetadata}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
